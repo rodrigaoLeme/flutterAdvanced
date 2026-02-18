2c6a1de463687080ffda9dc352c6ba859821c912
Rodrigo Leme <rodrigo.leme@iatec.com>
2026-02-18 00:24:52 -0300

    move NextEventPlayer to its own file

--- test/domain/entities/next_event_player_test.dart
+++ test/domain/entities/next_event_player_test.dart
@@ -1,63 +1,6 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter_advanced/domain/entities/next_event_player.dart';
+
 import 'package:flutter_test/flutter_test.dart';
-
-class NextEventPlayer {
-  final String id;
-  final String name;
-  final String initals;
-  final String? photo;
-  final String? position;
-  final bool isConfirmed;
-  final DateTime? confirmationDate;
-
-  const NextEventPlayer._({
-    required this.id,
-    required this.name,
-    required this.isConfirmed,
-    required this.initals,
-    this.photo,
-    this.position,
-    this.confirmationDate,
-  });
-
-  factory NextEventPlayer.fromName({
-    required String id,
-    required String name,
-    required bool isConfirmed,
-    String? photo,
-    String? position,
-    DateTime? confirmationDate,
-  }) {
-    return NextEventPlayer._(
-      id: id,
-      name: name,
-      initals: _getInitials(name),
-      isConfirmed: isConfirmed,
-      photo: photo,
-      position: position,
-      confirmationDate: confirmationDate,
-    );
-  }
-
-  static String _getInitials(String name) {
-    late final String firstChar;
-    late final String lastChar;
-
-    final names = name.toUpperCase().trim().split(' ');
-
-    if (names.first == '') {
-      firstChar = '-';
-      lastChar = '';
-    } else if (names.length == 1) {
-      firstChar = names.first[0];
-      lastChar = (names.first.split('').length > 1) ? names.first[1] : '';
-    } else {
-      firstChar = names.first[0];
-      lastChar = names.last[0];
-    }
-    return '$firstChar$lastChar';
-  }
-}
 
 // SUT = System Under Test ou OUT = Object Under Test
 
