182642cff4390b94f9a5e2f1deeb50e890410119
Rodrigo Leme <rodrigo.leme@iatec.com>
2026-02-19 13:57:05 -0300

    change Mock to Spy

--- test/domain/usecases/next_event_loader_test.dart
+++ test/domain/usecases/next_event_loader_test.dart
@@ -31,7 +31,7 @@ abstract class LoadNextEventRepository {
   Future<NextEvent> loadNextEvent({required String groupId});
 }
 
-class LoadNextEventMockRepository implements LoadNextEventRepository {
+class LoadNextEventSpyRepository implements LoadNextEventRepository {
   String? groupId;
   int callsCount = 0;
   NextEvent? output;
@@ -47,12 +47,12 @@ class LoadNextEventMockRepository implements LoadNextEventRepository {
 
 void main() {
   late String groupId;
-  late LoadNextEventMockRepository repo;
+  late LoadNextEventSpyRepository repo;
   late NextEventLoader sut;
 
   setUp(() {
     groupId = Random().nextInt(50000).toString();
-    repo = LoadNextEventMockRepository();
+    repo = LoadNextEventSpyRepository();
     repo.output = NextEvent(
       groupName: 'any group name',
       date: DateTime.now(),
