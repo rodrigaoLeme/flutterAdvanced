ddc4ee36bab6f253b91e69b6abd8995ada613f77
Rodrigo Leme <rodrigo.leme@iatec.com>
2026-02-24 00:48:48 -0300

    ensure LoadNextEventHttpRepository throws SessionExpiredError on 401

--- test/infra/repositories/load_next_event_http_repository_test.dart
+++ test/infra/repositories/load_next_event_http_repository_test.dart
@@ -11,7 +11,8 @@ import 'package:http/http.dart';
 import '../../helpers/fakes.dart';
 
 enum DomainError {
-  unexpected;
+  unexpected,
+  sessionExpired;
 }
 
 class LoadNextEventHttpRepository implements LoadNextEventRepository {
@@ -34,6 +35,7 @@ class LoadNextEventHttpRepository implements LoadNextEventRepository {
     final response = await httpClient.get(uri, headers: headers);
 
     if (response.statusCode == 400) throw DomainError.unexpected;
+    if (response.statusCode == 401) throw DomainError.sessionExpired;
     if (response.statusCode == 403) throw DomainError.unexpected;
     if (response.statusCode == 404) throw DomainError.unexpected;
     if (response.statusCode == 500) throw DomainError.unexpected;
@@ -193,6 +195,12 @@ void main() {
     httpClient.statusCode = 400;
     final future = sut.loadNextEvent(groupId: groupId);
     expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('should throw SessionExpiredError on 401', () async {
+    httpClient.statusCode = 401;
+    final future = sut.loadNextEvent(groupId: groupId);
+    expect(future, throwsA(DomainError.sessionExpired));
   });
 
   test('should throw UnexpectedError on 403', () async {
