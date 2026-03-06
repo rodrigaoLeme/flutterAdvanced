36d015fcc029b8f00cacdfd32f6581885224d3d3
Rodrigo Leme <rodrigo.leme@iatec.com>
2026-03-05 23:25:17 -0300

    ensure LoadNextEventApiRepository calls HttpClient with correct params

--- test/infra/api/repositories/load_next_event_api_repository_test.dart
+++ test/infra/api/repositories/load_next_event_api_repository_test.dart
@@ -9,21 +9,23 @@ class LoadNextEventApiRepository {
   LoadNextEventApiRepository({required this.httpClient, required this.url});
 
   Future<void> loadNextEvent({required String groupId}) async {
-    await httpClient.get(url: url);
+    await httpClient.get(url: url, params: {"groupId": groupId});
   }
 }
 
 abstract class HttpGetClient {
-  Future<void> get({required String url});
+  Future<void> get({required String url, Map<String, String>? params});
 }
 
 class HttpGetClientSpy implements HttpGetClient {
   String? url;
   int callsCount = 0;
+  Map<String, String>? params;
 
   @override
-  Future<void> get({required String url}) async {
+  Future<void> get({required String url, Map<String, String>? params}) async {
     this.url = url;
+    this.params = params;
     callsCount++;
   }
 }
@@ -41,9 +43,10 @@ void main() {
     sut = LoadNextEventApiRepository(httpClient: httpClient, url: url);
   });
 
-  test('should call httoClient with correct URL', () async {
+  test('should call httoClient with correct Input', () async {
     await sut.loadNextEvent(groupId: groupId);
     expect(httpClient.url, url);
+    expect(httpClient.params, {"groupId": groupId});
     expect(httpClient.callsCount, 1);
   });
 }
