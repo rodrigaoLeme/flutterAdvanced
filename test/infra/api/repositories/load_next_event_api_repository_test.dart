118ae4d556634d41fe5d3ab5b79bf5aa844d5ce5
Rodrigo Leme <rodrigo.leme@iatec.com>
2026-03-06 01:26:01 -0300

    create typealias to improve readability

--- test/infra/api/repositories/load_next_event_api_repository_test.dart
+++ test/infra/api/repositories/load_next_event_api_repository_test.dart
@@ -1,9 +1,13 @@
 import 'package:flutter_advanced/domain/entities/next_event.dart';
 import 'package:flutter_advanced/domain/entities/next_event_player.dart';
 import 'package:flutter_advanced/domain/repositories/load_next_event_repository.dart';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import '../../../helpers/fakes.dart';
+
+typedef Json = Map<String, dynamic>;
+typedef JsonArr = List<Json>;
 
 class LoadNextEventApiRepository implements LoadNextEventRepository {
   final HttpGetClient httpClient;
@@ -13,27 +17,26 @@ class LoadNextEventApiRepository implements LoadNextEventRepository {
 
   @override
   Future<NextEvent> loadNextEvent({required String groupId}) async {
-    final json = await httpClient
-        .get<Map<String, dynamic>>(url: url, params: {"groupId": groupId});
+    final json =
+        await httpClient.get<Json>(url: url, params: {"groupId": groupId});
 
     return NextEventMapper.toObject(json);
   }
 }
 
 class NextEventMapper {
-  static NextEvent toObject(Map<String, dynamic> json) => NextEvent(
+  static NextEvent toObject(Json json) => NextEvent(
       groupName: json['groupName'],
       date: DateTime.parse(json['date']),
       players: NextEventPlayerMapper.toList(json['players']));
 }
 
 class NextEventPlayerMapper {
-  static List<NextEventPlayer> toList(List<Map<String, dynamic>> arr) => arr
+  static List<NextEventPlayer> toList(JsonArr arr) => arr
       .map<NextEventPlayer>((player) => NextEventPlayerMapper.toObject(player))
       .toList();
 
-  static NextEventPlayer toObject(Map<String, dynamic> json) =>
-      NextEventPlayer.fromName(
+  static NextEventPlayer toObject(Json json) => NextEventPlayer.fromName(
         id: json['id'],
         name: json['name'],
         position: json['position'],
@@ -44,18 +47,18 @@ class NextEventPlayerMapper {
 }
 
 abstract class HttpGetClient {
-  Future<T> get<T>({required String url, Map<String, String>? params});
+  Future<T> get<T>({required String url, Json? params});
 }
 
 class HttpGetClientSpy implements HttpGetClient {
   String? url;
   int callsCount = 0;
-  Map<String, String>? params;
+  Json? params;
   dynamic response;
   Error? error;
 
   @override
-  Future<T> get<T>({required String url, Map<String, String>? params}) async {
+  Future<T> get<T>({required String url, Json? params}) async {
     this.url = url;
     this.params = params;
     callsCount++;
